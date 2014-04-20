b441e1875652ea2bd50a15143b8f4d8561bcfeeb
Shyam S Kumar <shyam.salim.kumar@outlook.com>
2014-04-20 14:21:57 +0530

    Updated the bash script.

--- setupSymlinks.sh
+++ setupSymlinks.sh
@@ -4,3 +4,5 @@ ln -s $PWD/.bowerrc ~/.bowerrc
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.gitignore ~/.gitignore
 ln -s $PWD/.ssh/config ~/.ssh/config
+ln -s $PWD/.jshintrc ~/.jshintrc
+ln -s $PWD/.jscs.json ~/.jscsrc
